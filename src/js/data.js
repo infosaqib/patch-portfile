ce19f3c909156659d75b555448face00b8864858
infosaqib <hafizsaqi555@gmail.com>
2024-02-28 17:37:03 +0500

    first commit

---
+++ src/js/data.js
@@ -0,0 +1,69 @@
+
+let serviceCardData = [
+    {
+      icon: "bi-upc-scan",
+      title: "Landing Page Design",
+      price: 25,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non odit asperiores ipsum doloremque impedit voluptas laudantium ad rem perferendis sunt officiis dolore",
+    },
+    {
+      icon: "bi-window-sidebar",
+      title: "Portfolio Website Design",
+      price: 50,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non odit asperiores ipsum doloremque impedit voluptas laudantium ad rem perferendis sunt officiis dolore",
+    },
+    {
+      icon: "bi-film",
+      title: "Whiteboard Animation",
+      price: 60,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non odit asperiores ipsum doloremque impedit voluptas laudantium ad rem perferendis sunt officiis dolore",
+    },
+    {
+      icon: "bi-megaphone-fill",
+      title: "Digital Marketing",
+      price: 30,
+      desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Non odit asperiores ipsum doloremque impedit voluptas laudantium ad rem perferendis sunt officiis dolore",
+    }
+  ];
+
+let portCardData = [
+    {
+      img: "src/images/port-img.webp",
+      title: "Portfolio Website",
+      subtitle: 'Development'
+    },
+    {
+      img: "src/images/shop-cart.webp",
+      title: "Shopping Cart",
+      subtitle: 'Project'
+    },
+    {
+      img: "src/images/furniture.webp",
+      title: "Ecommerce Website",
+      subtitle: 'Modification'
+    },
+    {
+      img: "src/images/port.webp",
+      title: "Cuberto Website",
+      subtitle: 'Clone'
+    },
+  ];
+
+
+  let testiCardData = [
+    {
+      name: 'Muhib',
+      role: 'freelancer',
+      img: "src/images/elon.webp",
+      desc: "I can't imagine managing our properties without this incredible Laravel system. It has saved us time,reduced errors, and improved overall productivity A must-have for any property management team"
+
+    },
+    {
+      name: 'Muhib',
+      role: 'freelancer',
+      img: "src/images/elon.webp",
+      desc: "I can't imagine managing our properties without this incredible Laravel system. It has saved us time,reduced errors, and improved overall productivity A must-have for any property management team"
+
+    },
+    
+  ];
